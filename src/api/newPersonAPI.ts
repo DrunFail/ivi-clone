cb19eabf5e64ee16bc4eea215bf9c764307cd115
Andrew Novikov <andrew.novikov@outlook.com>
2024-02-02 16:54:06 +0300

    refactor: PersonPage - changed the api call

--- src/api/newPersonAPI.ts
+++ src/api/newPersonAPI.ts
@@ -1,20 +1,22 @@
 import { axiosAuth } from "../lib/axios"
+import { ResponseWithCountAndRows } from "../models/response"
+import { DetailedPerson, Person } from "../models/types"
 
 export const NewPersonAPI = {
-    getAllPersonList: async (personQueryParams: {page?: number, size?: number,name?: string}) => {
-        const response = await axiosAuth.request({
+    getAllPersonList: async (personQueryParams?: {page?: number, size?: number,name?: string}) => {
+        const response = await axiosAuth.request<ResponseWithCountAndRows<Person>>({
             url: `/api/persons`,
             method: "get",
             params: personQueryParams
         })
-        return response
+        return response.data.rows
     },
     getPersonById: async (personId: number) => {
-        const response = await axiosAuth.request({
+        const response = await axiosAuth.request<DetailedPerson>({
             url: `/api/persons/about/${personId}`,
             method: "get"
         })
-        return response
+        return response.data
     },
     getPersonSuggest: async (personSuggestQueryParams: {profession: string, name: string, size?: number}) => {
         const response = await axiosAuth.request({
