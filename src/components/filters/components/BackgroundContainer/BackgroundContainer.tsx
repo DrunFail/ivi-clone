db404a9cb8a73b296ab85937208ca072d10e5459
Andrew Novikov <andrew.novikov@outlook.com>
2024-03-07 22:16:18 +0300

    refactor: InputFilterWithAutoSuggest

--- src/components/filters/components/BackgroundContainer/BackgroundContainer.tsx
+++ src/components/filters/components/BackgroundContainer/BackgroundContainer.tsx
@@ -1,5 +1,5 @@
 export default function BackgroundContainer({ children }: {children:React.ReactNode}) {
     return (
-        <div style={{ backgroundColor:"#312b45", borderRadius: "10px", padding: "15px",position: "relative",blockSize:"100%"} }>{children}</div>
+        <div style={{ backgroundColor:"#312b45", borderRadius: "10px", padding: "10px",position: "relative",blockSize:"100%"} }>{children}</div>
     );
 }
