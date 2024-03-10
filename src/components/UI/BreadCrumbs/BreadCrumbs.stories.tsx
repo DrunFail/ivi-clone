cb866c31a380c70ac21d55ac012a74cc9097adb6
Andrew Novikov <andrew.novikov@outlook.com>
2024-03-10 16:05:38 +0300

    feature: BreadCrumbs component

---
+++ src/components/UI/BreadCrumbs/BreadCrumbs.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BreadCrumbs from "./BreadCrumbs";
+
+const meta: Meta<typeof BreadCrumbs> = {
+    title: 'ui/BreadCrumbs/BreadCrumbs',
+    component: BreadCrumbs,
+    args: {
+        breadcrumbs: [
+            {
+                children: "title1",
+                href: ""
+            },
+            {
+                children: "title2",
+                href: ""
+            },
+            {
+                children: "title3",
+                href: ""
+            }
+        ],
+        isLastCrumbActive:false
+    }
+};
+
+export default meta;
+type Story = StoryObj<typeof BreadCrumbs>;
+
+export const Primary: Story = {}
---
+++ src/components/UI/BreadCrumbs/Crumb/Crumb.tsx
@@ -0,0 +1,14 @@
+import styles from "./Crumb.module.scss";
+interface CrumbProps {
+    children: React.ReactNode,
+    active:boolean
+}
+export default function Crumb({children,active }: CrumbProps) {
+    return (
+        <span
+            className={`${styles.crumb} ${styles[active ? "active" : "last"]}`}
+        >
+            {children}
+        </span>
+    );
+}
