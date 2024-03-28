54a14f244e163f8af76412d4a6fb80b60f5c0f7f
Andrew Novikov <andrew.novikov@outlook.com>
2024-03-28 17:25:30 +0300

    refactor: replace MobileNavBar

--- src/components/MobileNavBar/HamburgerMenuModal/components/ButtonTopContainer/ButtonTopContainer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./ButtonTopContainer.module.scss";
-
-export default function ButtonTopContainer({ children }: { children: React.ReactNode }) {
-    return (
-        <div className={styles.container} data-testId="btn-sub-humb">
-            {children }
-        </div>
-    );
-}
--- src/components/MobileNavBar/HamburgerMenuModal/components/NavBarModalTitle/NavBarModalTitle.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./NavBarModalTitle.module.scss";
-export default function NavBarModalTitle({ children }: { children: React.ReactNode }) {
-    return (
-        <div className={styles.text} data-testId="nav-title-hamb">
-            {children}
-        </div>
-    );
-}
