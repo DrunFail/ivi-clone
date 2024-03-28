e4b9735216ff876c8b2cf4a0cb50bcfc2df6cd29
Andrew Novikov <andrew.novikov@outlook.com>
2024-03-28 17:24:29 +0300

    refactor: header

--- src/components/Header/portal/HeaderPortalWrapper/HeaderPortalWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./HeaderPortalWrapper.module.scss";
-
-export default function HeaderPortalWrapper({ children }: { children: React.ReactNode }) {
-    return (
-        <div className={styles.container }>{children}</div>
-    );
-}
