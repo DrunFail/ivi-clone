aecb7e57ce7ab96e01770e68927940d446ddc839
Andrew Novikov <andrew.novikov@outlook.com>
2024-04-03 20:37:38 +0300

    fix: add env for analyze

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === "true",
+})
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -12,13 +17,5 @@ const nextConfig = {
         ]
     },
 }
-
-/*module.exports = nextConfig*/
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: true,
-})
-
-
 
 module.exports = withBundleAnalyzer(nextConfig)
